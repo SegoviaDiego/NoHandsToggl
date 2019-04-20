19a2ae0b0ffd22b1791eefbefe56fb1458099399
Diego Segovia <segoviadiegomatias@gmail.com>
2019-04-20 14:29:12 -0300

    START - Sending api_token to login endpoint

---
+++ src/components/auth/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>Login</h1>
+    <input type="text" placeholder="Toggl API Token" v-model="token"> 
+    <button v-on:click="login">Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component
+export default class Login extends Vue {
+  token: string = '';
+
+  login(){
+    axios.get('https://www.toggl.com/api/v8/me', {
+      auth: {
+        username: this.token,
+        password: 'api_token'
+      }
+    }).then((response)=>{
+      //TODO: Navigate to dashboard
+      console.log(response);
+    }).catch((err)=>{
+      //TODO: Show error to the user
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
