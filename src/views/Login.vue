a1ce0d2dd918b2f15029e2e86e1b7621fa3f0423
Diego Segovia <segoviadiegomatias@gmail.com>
2019-04-20 14:31:38 -0300

    Sending api_token to login endpoint (#1)

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid">
+    <LoginComponent/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import LoginComponent from '@/components/auth/login.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    LoginComponent,
+  },
+})
+export default class Login extends Vue {}
+</script>
